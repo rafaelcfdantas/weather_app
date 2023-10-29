408f93de2e08d7daa7e4e92a87fe4818cbd986f1
Rafael de Carvalho França Dantas <128332609+rafaelcfdantas@users.noreply.github.com>
2023-10-28 23:08:20 -0300

    feat: adds not found page and local storage

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,48 +1,54 @@
-const search_form = document.querySelector('#search__form');
-const search_input = document.querySelector('.header__input');
-const search_btn = document.querySelector('.header__btn');
+window.addEventListener('load', () => {
+    const search_form = document.querySelector('#search__form');
+    const search_btn = document.querySelector('.header__btn');
 
-search_form.addEventListener('submit', event => updateForecast(event));
-search_btn.addEventListener('click', event => updateForecast(event));
+    search_form.addEventListener('submit', async (event) => {event.preventDefault(); await updateForecast()});
+    search_btn.addEventListener('click', async (event) => {event.preventDefault(); await updateForecast()});
 
-function updateForecast(event) {
-    event.preventDefault();
+    const storedCity = localStorage.getItem('storedCity');
 
-    let search_text = search_input.value;
+    if (storedCity) {
+        updateForecast(storedCity);
+    }
+});
+
+async function updateForecast(search_text = false) {
+    if (!search_text) {
+        search_text = document.querySelector('.header__input').value;
+    }
 
     if (search_text == '') {
         return false;
     }
 
-    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${encodeURI(search_text)}&appid=df8247dcd9dd7cda212d9d442b63600e`).then(res => {
-        let city = res.data[0];
+    try {
+        const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(search_text)}&units=metric&lang=pt_br&appid=df8247dcd9dd7cda212d9d442b63600e`);
 
-        if (city != undefined) {
-            var city_name = city.name;
+        changeScreenData({
+            timezone: res.data.timezone / 60,
+            timestamp: new Date(res.data.dt * 1000),
+            sunrise: new Date(res.data.sys.sunrise * 1000),
+            sunset: new Date(res.data.sys.sunset * 1000),
+            weather_id: res.data.weather[0].id,
+            city: `${res.data.name} - ${res.data.sys.country}`,
+            temp: Math.round(res.data.main.temp),
+            humidity: res.data.main.humidity + '%',
+            wind: (res.data.wind.speed * 3.6).toFixed(1) + 'km/h',
+            icon: 'https://openweathermap.org/img/wn/' + res.data.weather[0].icon + '@2x.png'
+        });
 
-            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${city.lat}&lon=${city.lon}&units=metric&lang=pt_br&appid=df8247dcd9dd7cda212d9d442b63600e`).then(res => {
-                let data = {
-                    timezone: res.data.timezone / 60,
-                    timestamp: new Date(res.data.dt * 1000),
-                    sunrise: new Date(res.data.sys.sunrise * 1000),
-                    sunset: new Date(res.data.sys.sunset * 1000),
-                    weather_id: res.data.weather[0].id,
-                    city: city_name,
-                    temp: Math.round(res.data.main.temp),
-                    humidity: res.data.main.humidity + '%',
-                    wind: (res.data.wind.speed * 3.6).toFixed(1) + 'km/h',
-                    icon: 'https://openweathermap.org/img/wn/' + res.data.weather[0].icon + '@2x.png'
-                };
-
-                changeScreenData(data);
-            })
-        }
-    }).catch(err => {
+        localStorage.setItem('storedCity', search_text);
+    } catch (err) {
+        document.querySelector('section.weather').classList.remove('active');
+        document.querySelector('.no__weather').classList.add('active');
         console.log(err);
-    })
+    }
 }
 
 function changeScreenData(data) {
+    document.querySelector('section.weather').classList.add('active');
+    document.querySelector('.no__weather').classList.remove('active');
+    document.querySelector('.header__input').value = '';
     document.getElementById('weather__img').src = data.icon;
     document.getElementById('weather__city').innerText = data.city;
     document.getElementById('temp').innerText = data.temp;
@@ -57,7 +63,7 @@ function changeScreenData(data) {
         container.classList.add('rain');
     } else {
         // clear sky
-        updateTimezone(data);
+        syncTimezone(data);
 
         let actual_hour = data.timestamp.getHours();
         let sunrise_hour = data.sunrise.getHours();
@@ -75,12 +81,12 @@ function changeScreenData(data) {
     }
 }
 
-function updateTimezone(data) {
+function syncTimezone(data) {
     let time = data.timestamp;
     let sunrise = data.sunrise;
     let sunset = data.sunset;
 
-    time.setMinutes(time. getMinutes() + time.getTimezoneOffset() + data.timezone);
-    sunrise.setMinutes(sunrise. getMinutes() + sunrise.getTimezoneOffset() + data.timezone);
-    sunset.setMinutes(sunset. getMinutes() + sunset.getTimezoneOffset() + data.timezone);
+    time.setMinutes(time.getMinutes() + time.getTimezoneOffset() + data.timezone);
+    sunrise.setMinutes(sunrise.getMinutes() + sunrise.getTimezoneOffset() + data.timezone);
+    sunset.setMinutes(sunset.getMinutes() + sunset.getTimezoneOffset() + data.timezone);
 }
